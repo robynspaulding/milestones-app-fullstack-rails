635a2e92508cf0249c398a6d4081e8f815007339
RobynSpaulding <robynm27@gmail.com>
2022-09-29 12:31:38 -0700

    adds photo new and create action, views index link and new file

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,5 +5,30 @@ class PhotosController < ApplicationController
     render template: "photos/index"
   end
 
+  def show
+    @photo = Photo.find_by(id: params[:id])
+    render template: "photos/show"
+  end
+
+  def new
+    @photo = Photo.new
+    render template: "photos/new"
+  end
   
+  def new
+    @photo = Photo.new
+    render template: "photos/new"
+  end
+  
+  def create
+    @photo = Photo.new(
+      image_url: params[:photo][:image_url],
+      description: params[:photo][:description],
+      date: params[:photo][:date],
+      kid_id: params[:photo][:kid_id],
+    )
+    @photo.save
+    redirect_to "/photos"
+  end
+
 end
